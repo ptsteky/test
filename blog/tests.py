58e2d270834d590cf5a5571d425b27a784a58c65
ptsteky <106725073+ptsteky@users.noreply.github.com>
2022-06-02 14:31:43 +0700

    Add files via upload

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,9 @@
-from django.test import TestCase, SimpleTestCase
+# from django.test import TestCase, SimpleTestCase
 
-# Create your tests here.
+# # Create your tests here.
 
 
-class SimpleTest(SimpleTestCase):
-    def test_blog_status(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
+# class SimpleTest(SimpleTestCase):
+#     def test_blog_status(self):
+#         response = self.client.get('/')
+#         self.assertEquals(response.status_code, 200)
