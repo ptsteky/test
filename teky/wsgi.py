e345fbebab59b7b234ffe11402406181675d5ad2
ptsteky <106725073+ptsteky@users.noreply.github.com>
2022-06-02 13:20:23 +0700

    Add files via upload

--- teky/wsgi.py
+++ teky/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teky.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teky.settings")
 
 application = get_wsgi_application()
